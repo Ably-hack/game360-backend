7afb769b86d8c519109218a65799eb4da664f467
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-20 07:09:04 +0100

    Bug fixes

--- index.js
+++ index.js
@@ -3,71 +3,66 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
-import mongoose from 'mongoose';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import IndexController from './controller/index_controller.js';
+import Database from './datastore/database.js';
+
 const app = express();
 const server = http.createServer(app);
 
-// The init function
-async function init() {
-  // Connect to the database
-  const { MONGO_URI } = process.env;
-  await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+class AppServer {
+    constructor() {
+        this.initializeAllProcesses();
+    }
 
-  // Initialize any other services or components that you need
+    async initializeAllProcesses() {
+        await this.initMiddleWare();
+        this.initializeCORS();
+        this.initializeDatabase();
+        this.initializeController();
+    }
+
+    async initializeDatabase() {
+        Database.connectDatabase(app);
+    }
+
+    async initMiddleWare() {
+        app.use('/static', express.static(path.join(__dirname, 'public')));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+    initializeCORS() {
+        var corsOptions = {
+            origin: function (origin, callback) {
+                callback(null, true)
+            }
+        }
+        app.use(cors(corsOptions));
+    };
+
+    initializeController() {
+        IndexController.initialize(app);
+    }
+
+    initializeApp() {
+        const port = process.env.PORT || 8000;
+        try {
+            server.listen(port, async () => {
+                console.log(`Application connected to port ${port}`);
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 }
 
-// The initializeAllProcesses function
-async function initializeAllProcesses() {
-  await this.initMiddleWare();
-  this.initializeCORS();
-  this.initializeRoutes();
-
-  // Call the init function
-  await init();
-}
-
-// The initMiddleWare function
-async function initMiddleWare() {
-  app.use('/static', express.static(path.join(__dirname, 'public')));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-}
-
-// The initializeCORS function
-function initializeCORS() {
-  var corsOptions = {
-    origin: function (origin, callback) {
-      callback(null, true);
-    },
-  };
-  app.use(cors(corsOptions));
-}
-
-// The initializeRoutes function
-function initializeRoutes() {
-  app.use('/auth', authRoutes);
-}
-
-// The initializeApp function
-async function initializeApp() {
-  const port = process.env.PORT || 8000;
-  try {
-    server.listen(port, async () => {
-      console.log(`Application connected to port ${port}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-initializeApp();
+let appServer = new AppServer();
+appServer.initializeApp(); 
