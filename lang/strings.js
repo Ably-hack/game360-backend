c6309e19df48c836b649330bc09b9f36b58f8423
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-20 07:24:46 +0100

    strings

---
+++ lang/strings.js
@@ -0,0 +1,11 @@
+const Strings = {
+    USER_ALREADY_EXISTS: 'User already exists',
+    ACCOUNT_CREATED: 'User Account created',
+    LOGIN_SUCCESSFUL: 'User login successful',
+    INVALID_CREDENTIALS: 'Invalid Credentials',
+    USER_NOT_FOUND: 'User not found',
+    EMAIL_NOT_FOUND: 'Email does not exists',
+    ERROR_RESPONSE: 'Something went wrong',
+}
+
+export default Strings;
