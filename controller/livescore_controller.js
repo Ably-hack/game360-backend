eeeae8b06a5a6ff6f9d89b8cb14257b32ea5f6bb
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-26 11:31:26 +0100

    controller: route validation

--- controller/livescore_controller.js
+++ controller/livescore_controller.js
@@ -1,9 +1,10 @@
 import Routes from "../routes/index_routes.js";
 import LiveScoreService from "../services/livescore_service.js";
+import AccountService from "../services/account_service.js";
 class LiveScoreController {
     static initialize(app) {
-        app.get(Routes.GET_LEAGUES, LiveScoreService.fetchLeague);
-        app.get(Routes.LEAGUE_TEAMS, LiveScoreService.fetchLeagueTeams);
+        app.get(Routes.GET_LEAGUES, AccountService.validateSession, LiveScoreService.fetchLeague);
+        app.get(Routes.LEAGUE_TEAMS, AccountService.validateSession, LiveScoreService.fetchLeagueTeams);
     }
 }
 
