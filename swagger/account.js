6f85028792e8a289ed35a29a4e9011f355ed3759
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-26 14:50:31 +0100

    swagger: account docs

---
+++ swagger/account.js
@@ -0,0 +1,46 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Account:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Account
+ * /v1/account/login:
+ *   post:
+ *     tags: [Account]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Account'
+ *     responses:
+ *       200:
+ *         description:
+ * 
+ * /v1/account/register:
+ *   post:
+ *     tags: [Account]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Account'
+ *     responses:
+ *       200:
+ *         description: 
+ */
