63819f7b6f5d221600a4bdc944eb4d28511f42e3
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-27 07:47:21 +0100

    swagger: league docs

--- swagger/index.js
+++ swagger/index.js
@@ -15,11 +15,6 @@ export const swaggerUIOptions = {
                 }
             }
         },
-        security: [
-            {
-                bearerAuth: [],
-            },
-        ],
     },
     apis: ["./swagger/*.js"],
 }
---
+++ swagger/league.js
@@ -0,0 +1,43 @@
+/**
+ * @swagger
+ * tags:
+ *   name: League
+ * /v1/livescore/get_leagues:
+ *   get:
+ *     security: 
+ *       - bearerAuth: []
+ *     tags: [League]
+ *     description: Retrieve information about a specific league.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ * 
+ * /v1/livescore/teams/{league_id}:
+ *   get:
+ *     parameters:
+ *       - in: path
+ *         name: league_id
+ *         required: true
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [League]
+ *     summary: Get League Team Information
+ *     description: Retrieve information about a specific league team.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ * 
+ * /v1/livescore/team/{team_id}:
+ *   get:
+ *     parameters: 
+ *       - in: path
+ *         name: team_id 
+ *         required: true
+ *     security: 
+ *       - bearerAuth: []
+ *     tags: [League]
+ *     description: Retrieve information about a specific team.
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
