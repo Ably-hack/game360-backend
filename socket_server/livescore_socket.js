f243dcf1124e7eff8326f55142a52a98b572cfb6
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-11-01 08:17:10 +0100

    socket: bug fixes

--- socket_server/livescore_socket.js
+++ socket_server/livescore_socket.js
@@ -1,17 +1,17 @@
-import WebSocket from "ws"
+import WebSocket from "ws";
 import LiveScoreRepository from "../repository/livescore_repo.js";
-import LiveScoreModel from "../models/livescore_model.js";
 import MessageBrokerService from "../services/message_broker.js";
+import LiveScoreModel from "../models/livescore_model.js";
 
 class LiveScoreServer {
     constructor() {
-        this.socketUrl = `wss://wss.apifootball.com/livescore?action=livescore&APIkey=${process.env.API_FOOTBALL_KEY}`;
+        this.socketUrl = `wss://wss.apifootball.com/livescore?action=get_events&APIkey=${process.env.API_FOOTBALL_KEY}`;
         this.webSocket = new WebSocket(this.socketUrl);
         this.messageBroker = new MessageBrokerService()
         this.handleOpenSocketConnection();
         this.handleSocketData();
-        this.handleCloseConnection();
-        this.handleErrorConnection();
+        // this.handleCloseConnection();
+        // this.handleErrorConnection();
     }
 
     handleOpenSocketConnection() {
@@ -34,27 +34,13 @@ class LiveScoreServer {
                 if (Array.isArray(livescoreData) && livescoreData?.length) {
                     for (let stat of livescoreData) {
                         const match_id = stat?.match_id;
-                        let matchExists = await LiveScoreRepository.fetchMatchId(match_id);
-                        if (matchExists) {
-                            return
+                        let matchData = await LiveScoreRepository.fetchMatchId(match_id);
+                        if (matchData) {
+                            return;
                         } else {
-                            const newLiveMatch = await LiveScoreModel.create({
-                                match_id: stat?.match_id,
-                                country_id: stat?.country_id,
-                                country_name: stat?.country_name,
-                                league_id: stat?.league_id,
-                                league_name: stat?.league_name,
-                                match_date: stat?.match_date,
-                                match_status: stat?.match_status,
-                                hometeam_id: stat?.match_hometeam_id,
-                                awayteam_id: stat?.match_awayteam_id,
-                                hometeam_name: stat?.match_hometeam_name,
-                                awayteam_name: stat?.match_awayteam_name,
-                                hometeam_goal: stat?.match_hometeam_score,
-                                awayteam_goal: stat?.match_awayteam_score,
-                                stadium: stat?.match_stadium
-                            });
-                            await newLiveMatch.save();
+                            await LiveScoreRepository.deleteExistingMatches();
+                            let liveMatches = await LiveScoreRepository.createLiveScoreMatches(stat);
+                            await liveMatches.save();
                         }
                     }
                 }
